e65a4074e3eac5fd6c87793ccacce226efc3d29c
felix_susanto <felix.susanto2000@gmail.com>
2021-09-08 22:38:58 +0800

    basic footer done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
+import Container from '../Container';
+import logo from '../../assets/images/logo.svg';
 
 const FooterWrapper = styled.footer`
   background: #2C0346;
   color: #fff;
-
+  padding: 50px 0;
+  .logo-wrapper {
+    display: flex;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1;
+    align-items: center;
+    > img {
+      margin-right: 1em;
+    }
+  }
+  .title, .nav-item {
+    cursor: pointer;
+  }
+  .title {
+    font-weight: 700;
+    padding: 1.5em 0; 
+  }
+  .left {
+    flex: 1;
+  }
+  .right {
+    display: flex;
+    flex: 2;
+    justify-content: space-between;
+    .nav-item {
+      + .nav-item {
+        margin-top: 12px;
+      }
+    }
+  }
+  ${Container} {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 const FooterNav = [
@@ -43,7 +79,24 @@ const FooterNav = [
 const Footer = () => {
   return (
     <FooterWrapper>
-      Something here
+      <Container>
+        <div className="left">
+          <div className="logo-wrapper">
+            <img src={logo} alt="mintable logo" /> Mintable
+          </div>
+          <div className="title">Join our community</div>
+        </div>
+        <div className="right">
+          {FooterNav.map((item, index) => {
+            return (
+              <section className="section-nav">
+                <div className="title">{item.title}</div>
+                {item.items.map((navitem) => <div className="nav-item">{navitem}</div>)}
+              </section>
+            );
+          })}
+        </div>
+      </Container>
     </FooterWrapper>
   );
 };
